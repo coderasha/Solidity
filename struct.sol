abf64d9445d9815f81889ba7557895d4cd155114
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2024-02-22 00:03:36 +0530

    Update struct.sol

--- struct.sol
+++ struct.sol
@@ -28,3 +28,28 @@ contract empDetails{
 ---------------------------------------------------------------------------------------------------------
 2nd Way
 
+pragma solidity ^0.8.0;
+
+struct Employee{
+        string name;
+        uint id;
+}
+
+contract empDetails{
+
+       Employee public e1 = Employee({
+                              name:"Asha",
+                              id:1
+
+
+                               });
+
+     function change(string memory _name, uint _id) public{
+
+        e1.name=_name;
+        e1.id = _id;
+     }                          
+
+
+
+
