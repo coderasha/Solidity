b5c35ed2e30d3c6dcff4473cef70bd13c3ba2156
Ashadipta Pati <44902407+coderasha@users.noreply.github.com>
2024-03-11 20:37:06 +0530

    Update Random.sol
    comment

--- Random.sol
+++ Random.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 contract random{
 
     uint  public randNum ;
-//The below function will crete a random number
+//The below function will crete a random number.
 function generateRand()public returns(uint){
     randNum = uint (keccak256(abi.encodePacked(msg.sender, block.timestamp, randNum)))%1000;
     return randNum;
